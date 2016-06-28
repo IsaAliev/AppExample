efd4a88e76a2190e9e2ebde51e90a593aef86d0b
IsaAliev <isaaliev12@gmail.com>
2016-06-28 17:20:17 +0300

    Initial Commit

---
+++ AppExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppExample
+//
+//  Created by user on 28.06.16.
+//  Copyright © 2016 I&N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AppExample
+//
+//  Created by user on 28.06.16.
+//  Copyright © 2016 I&N. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
